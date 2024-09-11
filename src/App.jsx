f664bb4b336dcbfba8f4997e685c897426ac5f75
Ricch.dev <anekeaustin@gmail.com>
2024-09-11 23:42:39 +0100

    initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import Name from './components/Name'
+import Price from './components/Price'
+import Image from './components/Image'
+import Description from './components/Description'
+
+
+
+function App() {
+
+  return (
+    <>
+    <div>
+       <Name/>
+       <Price/>
+       <Image/>
+       <Description/>
+    </div>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/product.js
@@ -0,0 +1,8 @@
+const product = {
+  name: 'Iphone 16',
+  price: "$799",
+  desc: "Apple's Flagship Phone",
+  imgUrl:"https://www.stuff.tv/wp-content/uploads/sites/2/2024/09/iPhone-16-colour-options.jpg?w=1024"
+}
+
+export default product
